99219eecfef46cf22dde37bad4a60a7a40fb4ec1
JeffereyCI <jefferey.c.i212@gmail.com>
2025-05-28 01:43:45 +0700

    feat: Add Navigation bar component

--- src/lib/component/Navbar.svelte
+++ src/lib/component/Navbar.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
-	import { Cat } from 'phosphor-svelte';
 	import { page } from '$app/stores';
 </script>
 
 <nav class="flex h-16 items-center justify-end gap-3 bg-gray-600 text-zinc-100 pr-5">
 	<a
 		href="/services"
-		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/recommend-fields'
+		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/services'
 			? 'border-orange-500 hover:border-b-orange-500'
 			: 'border-transparent hover:border-b-gray-900'} "
 	>
@@ -14,13 +13,10 @@
 	>
 	<a
 		href="/history"
-		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/personality-test'
+		class="flex h-full items-center border-b-2 px-4 {$page.url.pathname === '/history'
 			? 'border-orange-500 hover:border-b-orange-500'
 			: 'border-transparent hover:border-b-gray-900'} "
 	>
 		History</a
 	>
-
-	<Cat class="size-6" />
-
 </nav>
