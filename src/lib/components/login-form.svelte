9af17f5334fb865556d62dbd3b61d5846ea2ca37
ekaadev <ekaadev9@gmail.com>
2025-05-31 20:13:05 +0700

    feat(auth): implement LoginForm component for streamlined login process

---
+++ src/lib/components/login-form.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+	import * as Card from '@/components/ui/card/index';
+	import { Label } from '@/components/ui/label/index';
+	import { Input } from '@/components/ui/input/index';
+	import { Button } from '@/components/ui/button/index';
+	import { cn } from '$lib/utils';
+	import type { HTMLAttributes } from 'svelte/elements';
+	import image from '$lib/assets/placeholder.svg';
+
+	let { class: className, ...restProps }: HTMLAttributes<HTMLDivElement> = $props();
+</script>
+
+<div class={cn('flex flex-col gap-6', className)} {...restProps}>
+	<Card.Root class="overflow-hidden p-0">
+		<Card.Content class="grid p-0 md:grid-cols-2">
+			<form class="p-6 md:p-8" action="?/login" method="post">
+				<div class="flex flex-col gap-6">
+					<div class="flex flex-col items-center text-center">
+						<h1 class="text-2xl font-bold">Welcome back</h1>
+						<p class="text-muted-foreground text-balance">Login to your Clean Shoe account</p>
+					</div>
+					<div class="grid gap-3">
+						<Label for="email">Email</Label>
+						<Input id="email" type="email" placeholder="m@example.com" required name="email" />
+					</div>
+					<div class="grid gap-3">
+						<div class="flex items-center">
+							<Label for="password">Password</Label>
+							<a href="##" class="ml-auto text-sm underline-offset-2 hover:underline">
+								Forgot your password?
+							</a>
+						</div>
+						<Input id="password" type="password" required name="password" />
+					</div>
+					<Button type="submit" class="w-full">Login</Button>
+					<div
+						class="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
+					>
+						<span class="bg-card text-muted-foreground relative z-10 px-2"> Or continue with </span>
+					</div>
+					<Button type="submit" class="w-full" variant="outline" formaction="?/signup"
+						>Sign Up</Button
+					>
+				</div>
+			</form>
+			<div class="bg-muted relative hidden md:block">
+				<img
+					src={image}
+					alt="placeholder"
+					class="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+				/>
+			</div>
+		</Card.Content>
+	</Card.Root>
+	<div
+		class="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4"
+	>
+		By clicking continue, you agree to our <a href="##">Terms of Service</a> and
+		<a href="##">Privacy Policy</a>.
+	</div>
+</div>
--- src/routes/auth/+page.svelte
+++ src/routes/auth/+page.svelte
@@ -1,71 +1,9 @@
 <script lang="ts">
-  import { Button } from 'bits-ui';
-  import image from '$lib/assets/placeholder.svg';
-  import Input from '$lib/components/Input.svelte';
+	import LoginForm from '$lib/components/login-form.svelte';
 </script>
 
-<div class="flex min-h-svh w-full items-center justify-center bg-gray-50 flex-col p-6 md:p-10">
-  <div
-    class="w-full flex max-w-sm md:max-w-3xl rounded-md bg-white shadow-sm justify-between items-center">
-    <div class="flex flex-col gap-6">
-      <div class="overflow-hidden">
-        <div class="grid p-0 md:grid-cols-2">
-          <form class="p-6 md:p-8" action="?/login" method="post">
-            <div class="flex flex-col gap-6">
-              <div class="text-center flex flex-col items-center">
-                <p class="font-bold text-2xl">Welcome back</p>
-                <p class="font-thin text-gray-500">Login pake akun clean-shoe mu!</p>
-              </div>
-              <div class="space-y-2">
-                <label for="email" class="block text-gray-900 text-sm font-medium">Email</label>
-                <Input
-                  id="email"
-                  name="email"
-                  placeholder="Masukkan email mu!"
-                  type="email"
-                  required={true}
-                  variant="default"
-                />
-              </div>
-              <div class="space-y-2">
-                <div class="flex justify-between items-center">
-                  <label for="password" class="text-gray-900 text-sm font-medium">Password</label>
-                  <a href="/auth" class="text-gray-900 text-sm font-medium hover:underline">Lupa password?</a>
-                </div>
-                <Input
-                  id="password"
-                  name="password"
-                  placeholder="Masukkan password rahasia mu!"
-                  type="password"
-                  required={true}
-                  variant="default"
-                />
-              </div>
-              <Button.Root
-                type="submit"
-                class="w-full h-9 cursor-pointer rounded-sm bg-blue-300 px-4 py-2 text-center text-sm font-medium text-white shadow-sm hover:bg-blue-400 transition-all duration-300"
-              >
-                Masuk gak nih?
-              </Button.Root>
-              <div class="relative flex items-center ">
-                <div class="flex-grow border-t border-gray-300"></div>
-                <span class="mx-4 text-sm text-gray-500">Or</span>
-                <div class="flex-grow border-t border-gray-300"></div>
-              </div>
-              <Button.Root
-                formaction="?/signup"
-                type="submit"
-                class="w-full h-9 cursor-pointer rounded-sm bg-gray-50 px-4 py-2 text-center text-sm font-medium text-gray-700 shadow-xs hover:bg-gray-100 transition-all duration-300 border border-gray-200"
-              >
-                Daftar dulu ah
-              </Button.Root>
-            </div>
-          </form>
-          <div class="w-full relative hidden md:block">
-            <img src={image} alt="placeholder" class=" w-full h-full object-cover rounded-e-md" />
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+	<div class="w-full max-w-sm md:max-w-3xl">
+		<LoginForm />
+	</div>
 </div>
