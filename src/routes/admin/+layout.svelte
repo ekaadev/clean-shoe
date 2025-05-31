adcd2abf20ae173e7242b9c25654b6a6de8df70a
ekaadev <ekaadev9@gmail.com>
2025-05-31 20:19:24 +0700

    feat: implement admin layout with sidebar and page structure

---
+++ src/routes/admin/+layout.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import AppSidebar from '$lib/components/app-sidebar.svelte';
+	import SiteHeader from '$lib/components/site-header.svelte';
+
+	const { children } = $props();
+</script>
+
+<Sidebar.Provider
+	style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+>
+	<AppSidebar variant="inset" />
+	<Sidebar.Inset>
+		<SiteHeader />
+		<div class="p-4">
+			{@render children()}
+		</div>
+	</Sidebar.Inset>
+</Sidebar.Provider>
--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,6 +1,5 @@
 <script>
-  let { children } = $props();
-
+	let { children } = $props();
 </script>
 
 {@render children()}
