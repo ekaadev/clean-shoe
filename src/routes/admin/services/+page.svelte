3363e016eae7b9edb9dd9ffb9ba8f69dc86170dc
ekaadev <ekaadev9@gmail.com>
2025-06-07 22:56:02 +0700

    feat: restructure service management layout for improved UI organization

--- src/routes/admin/services/+page.svelte
+++ src/routes/admin/services/+page.svelte
@@ -2,4 +2,10 @@
 	<title>Service Management</title>
 </svelte:head>
 
-<h1>Halo From Services</h1>
+<div class="flex flex-1 flex-col">
+	<div class="@container/main flex flex-1 flex-col gap-2">
+		<div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+			<h1>Halo From Services</h1>
+		</div>
+	</div>
+</div>
