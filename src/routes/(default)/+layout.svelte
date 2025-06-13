ffd6b86dbd0a6c60e290aecf7d65792830d4bbc5
ekaadev <ekaadev9@gmail.com>
2025-06-13 19:16:47 +0700

    feat: add layout and page components with navigation and footer integration

---
+++ src/routes/(default)/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import NavMenu from '@/components/nav-menu.svelte';
+	import Footer from '@/components/footer.svelte';
+	const { children } = $props();
+</script>
+
+<div class="flex flex-1 flex-col">
+	<NavMenu />
+	<div>
+		{@render children()}
+	</div>
+	<!-- footer -->
+	<Footer />
+</div>
