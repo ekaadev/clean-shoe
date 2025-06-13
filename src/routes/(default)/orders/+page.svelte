e06df5274692d7bcc7dd452e7cda4fd73f7a5648
ekaadev <ekaadev9@gmail.com>
2025-06-13 19:17:05 +0700

    feat: create orders page with basic layout and title

---
+++ src/routes/(default)/orders/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Orders</title>
+</svelte:head>
+
+<div class="flex flex-1 flex-col">
+	<div class="container mx-auto flex flex-1 flex-col">
+		<h1>Orders</h1>
+	</div>
+</div>
